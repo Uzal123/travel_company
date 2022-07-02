2d48bc9a573aff7d7e7d133e6c7ac37102969097
Ujjwal-1214 <ujwalpaswan1214@gmail.com>
2022-07-02 16:31:47 +0530

    initial

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: {
+                and: [/\.(js|ts|md)x?$/]
+            },
+            use: [{loader: '@svgr/webpack',options: { icon:true } }],
+        });
+        return config;
+    },
 }
 
 module.exports = nextConfig
+
